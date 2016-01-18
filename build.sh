97c96c56e82defa8ab4e83f473c8ff13a665c359
labnize <846800668@qq.com>
2016-01-18 14:20:05 +0800

    bulid.sh modify

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@ hexo generate #生成静态整站
 cd ./public #生成的静态页面会存储在public目录下
 git init
 git config --global push.default matching
-git config --global user.email "username@example.com" #填入GitHub的邮箱地址
-git config --global user.name "username" #填入GitHub的用户名
+git config --global user.email "846800668@qq.com" #填入GitHub的邮箱地址
+git config --global user.name "labnize" #填入GitHub的用户名
 git add --all .
 git commit -m "Travis CI Auto Builder" #自动构建后的内容将全部以此信息提交
 git push --force https://${GitHubKEY}@github.com/labnize/labnize.github.io master  #自动构建后的内容将全部以此信息提交
